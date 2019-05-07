8e1f6fd7a48ec355fd74e210084ebf5340d773e4
Manjel Villa <manuelfervi99@gmail.com>
2019-05-07 15:01:33 +0200

    CREACIÓN DE MAIN

--- src/conversordeunidades/ConversorDeUnidades.java
+++ src/conversordeunidades/ConversorDeUnidades.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package conversordeunidades;
 
-/**
- *
- * @author DAM102
- */
+import java.util.Scanner;
+
 public class ConversorDeUnidades {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        
+        int opc;
+        double pulgadas, milimetros, yardas, metros, millas, kilometros;
+        
+        System.out.println("Ingrese la opción que desea:");
+        
+        System.out.println("1 - Pulgadas a milímetros.");
+        System.out.println("2 - Yardas a metros.");
+        System.out.println("3 - Millas a Kilómetros.");
+        
+        opc = leer.nextInt();
+        
+        switch(opc){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
     }
     
 }
