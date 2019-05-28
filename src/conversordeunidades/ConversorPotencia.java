a4fc20920b02aa65fd3eb7cd5b5291e55c172b5e
Marcos Cardeiro <marcoscardeiro@gmail.com>
2019-05-28 14:26:21 +0200

    Ahora sí que se ha arreglado todo

--- src/conversordeunidades/ConversorPotencia.java
+++ src/conversordeunidades/ConversorPotencia.java
@@ -5,7 +5,7 @@ public class ConversorPotencia {
   public double ConversorPotencias(double distancia,int opc1,int opc2){
       double[] conversor1={1,1000,735.49,745.7,1000000,1.3558};
       double[] conversor2={1,0.001,0.001359,0.00134102,0.000001,0.737562};
-      distancia=((distancia*conversor1[opc1])*conversor2[opc2]);
+      distancia=((distancia*conversor1[opc1-1])*conversor2[opc2-1]);
     return distancia;  
   } 
 }
--- src/conversordeunidades/ConversorTemperatura.java
+++ src/conversordeunidades/ConversorTemperatura.java
@@ -5,7 +5,7 @@ public class ConversorTemperatura {
   public double ConversorTemperaturas(double distancia,int opc1,int opc2){
       double[] conversor1={1,((5)-32)/9,(1)+273.15,((5)-491.67)/9,((1)+100)/1.500,0.33000,(5)/4,((21)/40)+7.5};
       double[] conversor2={1,((9)+32)/5,(1)-273.15,((9)+491.67)/5,(((1)-100)*3)/2,(1)/0.33000,(4)/5,((1)-7.5)/0.52500};
-      distancia=((distancia*conversor1[opc1])*conversor2[opc2]);
+      distancia=((distancia*conversor1[opc1-1])*conversor2[opc2-1]);
     return distancia;  
   } 
 }
